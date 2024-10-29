a2bfe6679eaebea59725a4a3bbb1c492ae48b387
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-10-28 22:14:25 -0500

    feat: added new function that makes a request to lookup for users email

---
+++ app/fetchRequests/emailLookup.tsx
@@ -0,0 +1,6 @@
+const emailLookup = async (email: string)=>{
+    return await fetch(`http://localhost:8080/accounts/login/${email}/email-lookup/`, {
+        method: 'GET',
+    })
+}
+export default emailLookup
