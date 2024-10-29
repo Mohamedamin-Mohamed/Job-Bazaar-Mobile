5156073c8bca793eb5bbef48815347abfde2f21c
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-10-28 22:16:25 -0500

    feat: added new screen navigations

--- app/App.tsx
+++ app/App.tsx
@@ -1,9 +1,12 @@
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
-import Signup from "@/app/Navbars/Signup";
-import Login from "@/app/Navbars/Login";
+
 import HomePanel from "@/app/Panels/HomePanel";
 import CreateAccountPanel from "@/app/Panels/CreateAccountPanel";
 import LoginPanel from "@/app/Panels/LoginPanel";
+import Signup from "@/app/Auth/Signup";
+import Login from "@/app/Auth/Login";
+import ForgotPassword from "@/app/Panels/ForgotPassword";
+import ResetPassword from "@/app/Panels/ResetPassword";
 
 const Stack = createNativeStackNavigator()
 const App = () => {
@@ -12,8 +15,10 @@ const App = () => {
             <Stack.Screen options={{headerTitle: ''}} initialParams={{usrEmail: ''}} name="HomePanel" component={HomePanel}/>
             <Stack.Screen options={{headerTitle: ''}} name="Signup" component={Signup}/>
             <Stack.Screen options={{headerTitle: ''}} name="Login" component={Login}/>
-            <Stack.Screen name={"CreateAccount"} component={CreateAccountPanel}/>
-            <Stack.Screen options={{title: ''}} name={"LoginPanel"} component={LoginPanel}/>
+            <Stack.Screen options={{title: ''}} name="CreateAccount" component={CreateAccountPanel}/>
+            <Stack.Screen options={{title: ''}} name="LoginPanel" component={LoginPanel}/>
+            <Stack.Screen options={{title: ''}} name="ForgotPassword" component={ForgotPassword}/>
+            <Stack.Screen options={{title: ''}} name="ResetPassword" component={ResetPassword}/>
         </Stack.Navigator>
     )
 }
