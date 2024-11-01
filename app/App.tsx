bd785938c2f1f7dd2452ed0066f3ffbe700cf568
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-10-31 20:17:06 -0500

    feat: added new screens

--- app/App.tsx
+++ app/App.tsx
@@ -7,19 +7,34 @@ import Signup from "@/app/Auth/Signup";
 import Login from "@/app/Auth/Login";
 import ForgotPassword from "@/app/Panels/ForgotPassword";
 import ResetPassword from "@/app/Panels/ResetPassword";
+import CareerHub from "@/app/CareerHub/CareerHub";
+import {Provider} from "react-redux";
+import {persistor, store} from "@/app/Redux/store";
+import {PersistGate} from "redux-persist/integration/react";
+import UploadedJobs from "@/app/UploadedJobs/UploadedJobs";
+import {RootStackParamList} from "@/app/Types/types";
+import CareerInterests from "@/app/CareerHub/CareerInterests";
 
-const Stack = createNativeStackNavigator()
+const Stack = createNativeStackNavigator<RootStackParamList>()
 const App = () => {
     return (
-        <Stack.Navigator initialRouteName={"HomePanel"} >
-            <Stack.Screen options={{headerTitle: ''}} initialParams={{usrEmail: ''}} name="HomePanel" component={HomePanel}/>
-            <Stack.Screen options={{headerTitle: ''}} name="Signup" component={Signup}/>
-            <Stack.Screen options={{headerTitle: ''}} name="Login" component={Login}/>
-            <Stack.Screen options={{title: ''}} name="CreateAccount" component={CreateAccountPanel}/>
-            <Stack.Screen options={{title: ''}} name="LoginPanel" component={LoginPanel}/>
-            <Stack.Screen options={{title: ''}} name="ForgotPassword" component={ForgotPassword}/>
-            <Stack.Screen options={{title: ''}} name="ResetPassword" component={ResetPassword}/>
-        </Stack.Navigator>
+        <Provider store={store}>
+            <PersistGate persistor={persistor} loading={null}>
+                <Stack.Navigator initialRouteName={"HomePanel"}>
+                    <Stack.Screen options={{headerTitle: ''}} name="HomePanel"
+                                  component={HomePanel}/>
+                    <Stack.Screen options={{headerTitle: ''}} name="Signup" component={Signup}/>
+                    <Stack.Screen options={{headerTitle: ''}} name="Login" component={Login}/>
+                    <Stack.Screen options={{title: ''}} name="CreateAccount" component={CreateAccountPanel}/>
+                    <Stack.Screen options={{title: ''}} name="LoginPanel" component={LoginPanel}/>
+                    <Stack.Screen options={{title: ''}} name="ForgotPassword" component={ForgotPassword}/>
+                    <Stack.Screen options={{title: ''}} name="ResetPassword" component={ResetPassword}/>
+                    <Stack.Screen name="CareerHub" component={CareerHub}/>
+                    <Stack.Screen name="UploadedJobs" component={UploadedJobs}/>
+                    <Stack.Screen name="CareerInterests" component={CareerInterests}/>
+                </Stack.Navigator>
+            </PersistGate>
+        </Provider>
     )
 }
 export default App
--- app/Hooks/hooks.ts
+++
@@ -1,4 +0,0 @@
-import { TypedUseSelectorHook, useSelector as rawUseSelector } from 'react-redux';
-import {RootState} from "@/app/Types/types";
-
-export const useSelector: TypedUseSelectorHook<RootState> = rawUseSelector;
