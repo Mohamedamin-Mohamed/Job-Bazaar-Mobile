24ac6c5be32b3a648d8e0da0ca17ea50cef3c072
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-10-31 20:15:12 -0500

    feat: added new function that fetches available jobs

---
+++ app/fetchRequests/getAvailableJobs.ts
@@ -0,0 +1,19 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const getAvailableJobs = async (controller: AbortController)=>{
+    const token = await AsyncStorage.getItem('token')
+    console.log('Token is ', token)
+    if(!token){
+        console.error('No token available')
+        throw new Error('No token available')
+    }
+
+    return await fetch('http://localhost:8080/api/jobs/', {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`
+        },
+        signal: controller.signal
+    })
+}
+export default getAvailableJobs
