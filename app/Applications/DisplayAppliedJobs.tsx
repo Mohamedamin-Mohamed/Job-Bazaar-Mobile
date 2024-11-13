b936bef8f1a050f541b84f950aa286f18d29feae
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-11-12 18:44:07 -0600

    feat: new component that displays jobs applied by users

---
+++ app/Applications/DisplayAppliedJobs.tsx
@@ -0,0 +1,11 @@
+import {Application} from "@/app/Types/types";
+import {View, Text} from "react-native";
+
+const DisplayAppliedJobs = ({appliedJobs} : {appliedJobs: Application[]})=> {
+    return(
+        <View>
+            <Text>Under Construnction</Text>
+        </View>
+    )
+}
+export default DisplayAppliedJobs
