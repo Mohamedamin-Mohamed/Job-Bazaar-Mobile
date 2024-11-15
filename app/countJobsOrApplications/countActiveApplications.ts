a90396dc178a48c47ecccd492e5cfd58e2f47102
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-11-14 20:57:25 -0600

    chore: renamed function name

--- app/countJobsOrApplications/countActiveApplications.ts
+++ app/countJobsOrApplications/countActiveApplications.ts
@@ -1,6 +1,6 @@
 import {Application} from "@/app/Types/types";
 
-const countActivepplications = (applications: Application[]) => {
+const countActiveApplications = (applications: Application[]) => {
     return applications.filter(app => app.isActive === 'true').length
 }
-export default countActivepplications
+export default countActiveApplications
