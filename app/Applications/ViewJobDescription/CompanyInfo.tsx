7bb31dd128c31f4e34ab4bd87ef7175fddd36c34
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-12-08 20:51:38 -0600

    feat: component that displays info about the company

---
+++ app/Applications/ViewJobDescription/CompanyInfo.tsx
@@ -0,0 +1,50 @@
+import {StyleSheet, Text, View} from "react-native";
+
+const CompanyInfo = () => {
+    return (
+        <View style={styles.container}>
+            <View style={styles.childContainer}>
+                <Text style={styles.headerText}>Welcome</Text>
+                <Text style={styles.normalText}>Thank you for using Job Bazaar</Text>
+                <Text style={styles.normalText}>We appreciate your application. Track your progress and sign up for job
+                    alerts. Stay updated on
+                    new opportunities and continue
+                    exploring career options with us...</Text>
+                <Text style={styles.headerText}>About Us</Text>
+                <Text style={styles.normalText}>Job Bazaar is an innovative job application platform designed to connect
+                    employers with top talent
+                    seamlessly. Employers can easily post job openings,
+                    manage applications, and find the right candidates for their teams. Job seekers can explore a wide
+                    range of job opportunities, apply directly through
+                    the platform, and track their application status. Whether you're an employer looking to hire or a
+                    job seeker aiming to advance your career, Job Bazaar
+                    simplifies the recruitment process, making it efficient and user-friendly for everyone
+                    involved.</Text>
+            </View>
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    container: {
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    childContainer: {
+        backgroundColor: "white",
+        borderWidth: 1,
+        borderRadius: 4,
+        borderColor: "white",
+        padding: 30,
+        marginVertical: 20
+    },
+    headerText: {
+        fontSize: 20,
+        fontWeight: "bold",
+        marginVertical: 20
+    },
+    normalText: {
+        fontSize: 16,
+        letterSpacing: 0.5
+    }
+})
+export default CompanyInfo
