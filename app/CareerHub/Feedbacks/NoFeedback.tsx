9ed97903d667f8398f90e72acf7294ac5ade6aa4
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-12-08 19:40:54 -0600

    feat: added new component thats displayed when the user doesn't have any registered feedback for a job

---
+++ app/CareerHub/Feedbacks/NoFeedback.tsx
@@ -0,0 +1,27 @@
+import {Image, StyleSheet, Text, View} from "react-native";
+
+const NoFeedback = () => {
+    const img = require('../../Images/empty_self_chat.png')
+    return (
+        <View style={styles.container}>
+            <Image source={img}/>
+            <Text style={[styles.generalText, {fontSize: 20, fontWeight: "bold"}]}>No feedback requests</Text>
+            <Text style={[styles.generalText, {paddingVertical: 6}]}>There are no requests</Text>
+            <Text style={styles.generalText}>Matching your search criteria</Text>
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    container: {
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 10,
+        marginVertical: 10,
+        marginLeft: "9%"
+    },
+    generalText: {
+        color: "#69717f",
+        fontSize: 18
+    }
+})
+export default NoFeedback
