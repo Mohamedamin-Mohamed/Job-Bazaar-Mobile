84016b4ac214d842ee8b13997fddea8f5c6517a5
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-12-08 20:55:47 -0600

    feat: child component that displays applied jobs of a user

--- app/Applications/DisplayAppliedJobs.tsx
+++ app/Applications/DisplayAppliedJobs.tsx
@@ -1,43 +1,81 @@
 import {Application, RootStackParamList} from "../Types/types";
 import {StyleSheet, Text, TouchableOpacity, View} from "react-native";
-import {useState} from "react";
-import countActiveApplications from "../countJobsOrApplications/countActiveApplications";
-import countInactiveApplications from "../countJobsOrApplications/countInactiveApplications";
+import {useEffect, useState} from "react";
+import countApplications from "../countJobsOrApplications/countApplications";
 import Active from "./ApplicationStatus/Active";
 import InActive from "./ApplicationStatus/InActive";
-import {StackNavigationProp} from "@react-navigation/stack";
 import {NavigationProp} from "@react-navigation/core";
+import NoApplications from "@/app/Applications/ApplicationStatus/NoApplications";
+import CompanyInfo from "@/app/Applications/ViewJobDescription/CompanyInfo";
 
 type DisplayAppliedJobsProps = {
     navigation: NavigationProp<RootStackParamList, 'AppliedJobs'>,
-    appliedJobs: Application[]
+    appliedJobs: Application[],
+    refreshJobs: () => void
 }
 
-const DisplayAppliedJobs = ({navigation, appliedJobs}: DisplayAppliedJobsProps) => {
+const DisplayAppliedJobs = ({navigation, appliedJobs, refreshJobs}: DisplayAppliedJobsProps) => {
     const [activeButton, setActiveButton] = useState('active')
-    const activeApplications = countActiveApplications(appliedJobs)
-    const inActiveApplications = countInactiveApplications(appliedJobs)
+    const {active, inactive} = countApplications(appliedJobs)
+
+    useEffect(() => {
+        return navigation.addListener('focus', () => {
+            refreshJobs()
+        })
+
+    }, [navigation, refreshJobs]);
+
+    const renderedComponent =
+        activeButton === "active" ? (
+            <Active
+                appliedJobs={appliedJobs}
+                activeApplications={active}
+                navigation={navigation}
+                refreshJobs={refreshJobs}
+            />
+        ) : (
+            <InActive
+                appliedJobs={appliedJobs}
+                inActiveApplications={inactive}
+                navigation={navigation}
+            />
+        );
+
     return (
         <View style={styles.container}>
             <View style={styles.childContainer}>
                 <Text style={styles.headerText}>My Applications</Text>
-                <Text style={styles.text}>As the employer is evaluating your qualifications, we may contact you to
-                    provide additional information. Thank you for your interest and thank you for
-                    using Job Bazaar.</Text>
-                <View style={styles.activityButtons}>
-                    <View style={{flex: 1}}>
-                        <TouchableOpacity style={[styles.buttons, activeButton === 'active' && styles.buttonClicked]} onPress={()=> setActiveButton('active')}>
-                            <Text style={styles.buttonsText}>Active ({activeApplications})</Text>
-                        </TouchableOpacity>
-                    </View>
-                    <View style={{flex: 1}}>
-                        <TouchableOpacity style={[styles.buttons, activeButton !== 'active' && styles.buttonClicked]} onPress={()=> setActiveButton('inactive')}>
-                            <Text style={styles.buttonsText}>Inactive ({inActiveApplications})</Text>
-                        </TouchableOpacity>
-                    </View>
-                </View>
-                {activeButton === 'active' ? <Active appliedJobs={appliedJobs} activeApplications={activeApplications} navigation={navigation} /> :
-                    <InActive appliedJobs={appliedJobs} inActiveApplications={inActiveApplications} navigation={navigation} />}
+
+                {appliedJobs.length > 1 ? (
+                    <>
+                        <Text style={styles.text}>As the employer is evaluating your qualifications, we may contact you
+                            to
+                            provide additional information. Thank you for your interest and thank you for
+                            using Job Bazaar.</Text>
+                        <View style={styles.activityButtons}>
+                            <View style={{flex: 1}}>
+                                <TouchableOpacity
+                                    style={[styles.buttons, activeButton === 'active' && styles.buttonClicked]}
+                                    onPress={() => setActiveButton('active')}>
+                                    <Text style={styles.buttonsText}>Active ({active})</Text>
+                                </TouchableOpacity>
+                            </View>
+                            <View style={{flex: 1}}>
+                                <TouchableOpacity
+                                    style={[styles.buttons, activeButton !== 'active' && styles.buttonClicked]}
+                                    onPress={() => setActiveButton('inactive')}>
+                                    <Text style={styles.buttonsText}>Inactive ({inactive})</Text>
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                        {renderedComponent}
+                    </>
+                ) : (
+                    <NoApplications navigation={navigation}/>
+                )}
+            </View>
+            <View style={{justifyContent: "center", alignItems: "center", width: "90%"}}>
+                <CompanyInfo/>
             </View>
         </View>
     )
