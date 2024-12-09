65eb19e28bee66739195a52f0102101a18836bba
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-12-08 20:57:07 -0600

    feat: component that gives user options to view or withdraw an application

--- app/Applications/ApplicationStatus/ApplicationOptions.tsx
+++ app/Applications/ApplicationStatus/ApplicationOptions.tsx
@@ -1,10 +1,33 @@
-import {Text, View,StyleSheet} from "react-native";
+import {StyleSheet, Text, TouchableOpacity, View} from "react-native";
+import {Application, RootStackParamList} from "@/app/Types/types";
+import {NavigationProp} from "@react-navigation/core";
 
-const ApplicationOptions = () => {
+interface ApplicationOptionsProps {
+    navigation: NavigationProp<RootStackParamList, 'AppliedJobs'>,
+    application: Application,
+    handleClickClose: (jobId: string) => void,
+    withdrawApp?: (application: Application) => void
+}
+
+const ApplicationOptions = ({navigation, application, handleClickClose, withdrawApp}: ApplicationOptionsProps) => {
+
+    const viewDescription = () => {
+        //what the below line does is it closes the application options component
+        handleClickClose(application.jobId)
+        navigation.navigate('ViewDescription', {application})
+    }
     return (
-        <View style={styles.container}>
-            <Text style={styles.viewButton}>View Application</Text>
-            <Text style={styles.withdrawButton}>Withdraw Application</Text>
+        <View style={[styles.container, application.isActive !== 'true' && {left: 180}]}>
+            <View style={styles.childContainer}>
+                <TouchableOpacity onPress={() => viewDescription()}>
+                    <Text style={styles.viewButton}>View Application</Text>
+                </TouchableOpacity>
+                {application.isActive === 'true' &&
+                    <TouchableOpacity onPress={() => withdrawApp && withdrawApp(application)}>
+                        <Text style={styles.withdrawButton}>Withdraw Application</Text>
+                    </TouchableOpacity>
+                }
+            </View>
         </View>
     )
 }
@@ -13,21 +36,27 @@ const styles = StyleSheet.create({
         position: "absolute",
         left: 150,
         top: 30,
+        marginTop: 10
+    },
+    childContainer: {
+        zIndex: 100,
         backgroundColor: "white",
-        borderColor: "white",
-        borderWidth: 2,
+        borderColor: "grey",
+        borderWidth: 0.4,
         borderRadius: 4,
     },
     viewButton: {
         backgroundColor: "#0875e1",
         color: "white",
         textAlign: "center",
+        fontSize: 16,
         marginTop: 8,
         padding: 6,
-        fontWeight: "bold",
+        fontWeight: "500"
     },
     withdrawButton: {
         padding: 6,
+        fontSize: 16,
     }
 })
 export default ApplicationOptions
