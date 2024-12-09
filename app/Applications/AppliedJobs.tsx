a08edcacbdbdf200728e4b3d4903845d07953d3d
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-12-08 20:55:19 -0600

    feat: added new component that fetches applied jobs by a certain user with role applicant

--- app/Applications/AppliedJobs.tsx
+++ app/Applications/AppliedJobs.tsx
@@ -1,21 +1,55 @@
-import {RootStackParamList} from "../Types/types";
+import {Application, RootStackParamList, RootState} from "../Types/types";
 import {NativeStackScreenProps} from "@react-navigation/native-stack";
-import {ScrollView, StyleSheet, View} from "react-native";
+import {ActivityIndicator, ScrollView, StyleSheet, View} from "react-native";
 import NoTasks from "./NoTasks";
 import DisplayAppliedJobs from "./DisplayAppliedJobs";
+import getAppliedJobs from "@/app/fetchRequests/getAppliedJobs";
+import {useSelector} from "react-redux";
+import {useEffect, useState} from "react";
+import CompanyInfo from "@/app/Applications/ViewJobDescription/CompanyInfo";
 
 type AppliedJobsNavigationProp = NativeStackScreenProps<RootStackParamList, 'AppliedJobs'>
 
 const AppliedJobs = ({navigation, route}: AppliedJobsNavigationProp) => {
-    const {jobs} = route.params
+    let {jobs} = route.params
+    const applicantEmail = useSelector((state: RootState) => state.userInfo).email
+    const [loading, setLoading] = useState(false)
+    const [appliedJobs, setAppliedJobs] = useState<Application[]>(jobs)
+
+    const fetchAppliedJobs = async () => {
+        setLoading(true);
+        try {
+            const fetchJobs = await getAppliedJobs(applicantEmail, new AbortController());
+            const jobs = await fetchJobs.json()
+            setAppliedJobs(jobs)
+        } catch (error) {
+            console.error('Error fetching jobs:', error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        fetchAppliedJobs().catch(err => console.error(err))
+    }, []);
+
     return (
         <ScrollView>
-            <View>
-                <NoTasks/>
-                <DisplayAppliedJobs navigation={navigation} appliedJobs={jobs}/>
-            </View>
+            {loading ? <ActivityIndicator size="large" color="#367c2b" style={styles.activityBar}/> :
+                <View>
+                    <NoTasks/>
+                    <DisplayAppliedJobs navigation={navigation} appliedJobs={appliedJobs}
+                                        refreshJobs={fetchAppliedJobs}/>
+                </View>
+            }
         </ScrollView>
     )
 }
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    activityBar: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    }
+})
 export default AppliedJobs
