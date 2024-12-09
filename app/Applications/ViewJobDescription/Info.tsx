28eca3cad0e334c8395cd969150fba7c5931bd1c
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-12-08 20:52:07 -0600

    feat: component that outlines what the app is being used for

---
+++ app/Applications/ViewJobDescription/Info.tsx
@@ -0,0 +1,26 @@
+import {Text, View} from "react-native";
+
+const Info = () => {
+    return (
+        <View>
+            <Text style={{fontSize: 16, letterSpacing: 0.5}}>
+                JobBazaar is a comprehensive job search platform designed to connect job seekers with employers across
+                various industries. The platform offers a range of features, including:
+
+                Job Listings: A vast collection of job postings from various companies, categorized by industry,
+                location, and job type.
+                Application Tracking: Tools for users to apply for jobs, track their applications, and manage their job
+                search progress.
+                Employer Profiles: Detailed profiles for employers showcasing their company culture, job opportunities,
+                and other relevant information.
+                User Profiles: Personalized profiles for job seekers where they can manage their resumes, track
+                applications, and set job alerts.
+                Referral Programs: Features for users to refer others to job openings and track the status of their
+                referrals.
+                JobBazaar aims to streamline the job search process and provide a user-friendly experience for both job
+                seekers and employers.
+            </Text>
+        </View>
+    )
+}
+export default Info
