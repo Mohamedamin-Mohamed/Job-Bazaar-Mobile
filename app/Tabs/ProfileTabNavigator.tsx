beabc7347933cce8df139ed5eef0a1e4a8d4aa04
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-12-10 19:27:18 -0600

    feat: new tab component that renders the experience and career interests section of profile

--- app/Tabs/ProfileTabNavigator.tsx
+++ app/Tabs/ProfileTabNavigator.tsx
@@ -1,12 +1,31 @@
 import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import Experience from "@/app/Tabs/Experience/Experience";
+import CareerInterests from "@/app/Tabs/CareerInterests/CareerInterests";
+import Icon from "react-native-vector-icons/MaterialIcons";
 
 const Tab = createBottomTabNavigator()
 
 const ProfileTabNavigator = ()=> {
 
     return(
-        <Tab.Navigator>
-            </>
+        <Tab.Navigator screenOptions={({route}) => ({
+            headerShown: false,
+            tabBarLabelStyle: {fontSize: 20},
+            tabBarIcon: ({color, size}) => {
+                let iconName
+                if(route.name === 'Experience'){
+                    iconName = 'work'
+                }
+                else{
+                    iconName = 'interests'
+                }
+                return <Icon name={iconName} size={size} color={color} />
+        },
+
+        })}>
+            <Tab.Screen name="Experience" component={Experience} />
+            <Tab.Screen name="Career Interests" component={CareerInterests} />
+        </Tab.Navigator>
     )
 }
 export default ProfileTabNavigator
