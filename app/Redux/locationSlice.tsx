52829cb565ff6b4d9d08c71b672550432c50d3d2
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2025-01-26 19:46:15 -0600

    refractor: added location reducer

--- app/Redux/locationSlice.tsx
+++ app/Redux/locationSlice.tsx
@@ -1,5 +1,5 @@
 import {createSlice, PayloadAction} from "@reduxjs/toolkit";
-import {Location} from "../Types/types";
+import {Location} from "@/Types/types";
 
 const initialState: Location = {
     city: '',
