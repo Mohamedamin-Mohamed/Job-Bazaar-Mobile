a78a293bc52925ee83991ae27c1e7159673150e4
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2025-01-26 19:40:14 -0600

    feat: added refreshJobs prop to JobPosted component

--- app/Management/ManagementHub.tsx
+++ app/Management/ManagementHub.tsx
@@ -1,9 +1,8 @@
 import {ActivityIndicator, ScrollView, StyleSheet, View} from "react-native";
-import getUploadedJobs from "@/app/fetchRequests/getUploadedJobs";
+import getUploadedJobs from "@/app/FetchRequests/getUploadedJobs";
 import {useSelector} from "react-redux";
-import {Job, RootStackParamList, RootState} from "@/app/Types/types";
+import {Job, RootStackParamList, RootState} from "@/Types/types";
 import {useEffect, useState} from "react";
-import NoJobs from "@/app/404s/NoJobs";
 import JobPosted from "@/app/Management/JobPosted";
 import {StackScreenProps} from "@react-navigation/stack";
 import Toast from "react-native-toast-message";
@@ -13,7 +12,6 @@ type ManagementHubNavigationProp = StackScreenProps<RootStackParamList, 'Managem
 const ManagementHub = ({navigation}: ManagementHubNavigationProp) => {
     const userInfo = useSelector((state: RootState) => state.userInfo)
     const employerEmail = userInfo.email
-    const role = userInfo.role
     const [uploadedJobs, setUploadedJobs] = useState<Job[]>([])
     const [loading, setLoading] = useState(true)
 
@@ -39,21 +37,17 @@ const ManagementHub = ({navigation}: ManagementHubNavigationProp) => {
     return (
         <View style={styles.container}>
             <Toast/>
-            {loading ? (
+            {loading ?
                 <ActivityIndicator size="large" color="#367c2b" style={styles.activityBar}/>
-            ) : (
-                uploadedJobs.length > 0 ? (
-                    <ScrollView>
-                        <JobPosted
-                            navigation={navigation}
-                            uploadedJobs={uploadedJobs}
-                            refreshJobs={fetchUploadedJobs}
-                        />
-                    </ScrollView>
-                ) : (
-                    <NoJobs role={role} navigation={navigation}/>
-                )
-            )}
+                :
+                <ScrollView>
+                    <JobPosted
+                        navigation={navigation}
+                        uploadedJobs={uploadedJobs}
+                        refreshJobs={fetchUploadedJobs}
+                    />
+                </ScrollView>
+            }
         </View>
     );
 };
