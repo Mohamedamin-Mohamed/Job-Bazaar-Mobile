b7ee504db95a0541b12eb73e327e3c184c6d9328
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2025-01-26 19:07:47 -0600

    feat: added refresh of applied jobs when user deletes an application

--- app/Applications/ApplicationStatus/InActive.tsx
+++ app/Applications/ApplicationStatus/InActive.tsx
@@ -1,5 +1,5 @@
 import {StyleSheet, Text, TouchableOpacity, View} from "react-native";
-import {Application, RootStackParamList, RootState} from "../../Types/types";
+import {Application, RootStackParamList, RootState} from "@/Types/types";
 import Header from "./Header";
 import {useState} from "react";
 import ApplicationOptions from "./ApplicationOptions";
@@ -7,17 +7,15 @@ import NoApplications from "./NoApplicationsOrJobs";
 import {NavigationProp} from "@react-navigation/core";
 import {useSelector} from "react-redux";
 import updateApplication from "@/app/fetchRequests/updateApplication";
+import React from "react";
 
 type InActiveProps = {
     navigation: NavigationProp<RootStackParamList, 'AppliedJobs'>,
     appliedJobs: Application[],
     inActiveApplications: number,
     jobStatuses: Record<string, string>,
-    refreshJobs: () => void
 }
-const InActive = ({navigation, appliedJobs, inActiveApplications, jobStatuses, refreshJobs}: InActiveProps) => {
-    const [loading, setLoading] = useState(false)
-    const [clicked, setClicked] = useState<Record<string, boolean>>({})
+const InActive = ({navigation, appliedJobs, inActiveApplications, jobStatuses}: InActiveProps) => {
     const role = useSelector((state: RootState) => state.userInfo).role
 
     const parseDate = (dateString: string) => {
@@ -25,43 +23,15 @@ const InActive = ({navigation, appliedJobs, inActiveApplications, jobStatuses, r
         return new Date(year, month - 1, day);
     };
 
-    // sorting appliedJobs by applicationDate
+    // sorts appliedJobs by applicationDate
     const sortedAppliedJobs = [...appliedJobs].sort((a, b) => {
         const dateA = parseDate(a.applicationDate).getTime()
         const dateB = parseDate(b.applicationDate).getTime()
         return dateB - dateA; // sort descending
     })
 
-    const handleClick = (jobId: string) => {
-        if (clicked[jobId]) {
-            setClicked({[jobId]: false})
-            return
-        }
-        setClicked({[jobId]: true})
-    }
-
-    const handleClickClose = (jobId: string) => {
-        setClicked({[jobId]: false})
-    }
-
     const viewDescription = (application: Application) => {
         navigation.navigate('ViewApplicationDescription', {application: application})
-    }
-
-    const withdrawApplication = async (application: Application) => {
-        try {
-            setLoading(true)
-            const applicationStatus = 'Candidate Withdrew Interest'
-            const updateResponse = await updateApplication(application.applicantEmail, application.jobId, applicationStatus, new AbortController())
-            handleClick(application.jobId)
-            if (updateResponse.ok) {
-                refreshJobs()
-            }
-        } catch (err) {
-            console.error(err)
-        } finally {
-            setLoading(false)
-        }
     }
 
     return (
@@ -75,13 +45,6 @@ const InActive = ({navigation, appliedJobs, inActiveApplications, jobStatuses, r
                                 <TouchableOpacity onPress={() => viewDescription(application)}>
                                     <Text style={styles.positionText}>{application.position}</Text>
                                 </TouchableOpacity>
-                                <TouchableOpacity onPress={() => handleClick(application.jobId)}>
-                                    <Text style={{fontSize: 20}}>...</Text>
-                                </TouchableOpacity>
-                                {clicked[application.jobId] && (
-                                    <ApplicationOptions navigation={navigation} application={application}
-                                                        handleClickClose={handleClickClose} withdrawApp={withdrawApplication}/>
-                                )}
                             </View>
                         )
                     ))}
