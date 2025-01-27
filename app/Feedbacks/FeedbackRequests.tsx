020fffdc92ba4b53eb7d08aac2a0b08fb9f75195
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2025-01-26 19:32:26 -0600

    feat: added props to DateRange component

--- app/Feedbacks/FeedbackRequests.tsx
+++ app/Feedbacks/FeedbackRequests.tsx
@@ -1,8 +1,7 @@
 import {ScrollView, View} from "react-native";
 import {Feedback} from "@/Types/types";
-import NoFeedback from "@/app/CareerHub/Feedbacks/NoFeedback";
-import DateRange from "@/app/CareerHub/Feedbacks/DateRange";
-import FeedbackDetails from "@/app/CareerHub/Feedbacks/FeedbackDetails";
+import DateRange from "@/app/Feedbacks/DateRange";
+import FeedbackDetails from "@/app/Feedbacks/FeedbackDetails";
 import {useState} from "react";
 import {addMonths} from "date-fns";
 
@@ -16,13 +15,11 @@ const FeedbackRequests = ({feedbacks}: { feedbacks: Feedback[] }) => {
     }
     return (
         <View>
-            {feedbacks.length > 0 ? (
-                <ScrollView>
-                    <DateRange setStartDate={(date: Date) => setStartDate(date)}
-                               setEndDate={(date: Date) => setEndDate(date)} dates={dates}/>
-                    <FeedbackDetails dates={dates} feedbacks={feedbacks}/>
-                </ScrollView>
-            ) : <NoFeedback/>}
+            <ScrollView>
+                <DateRange setStartDate={(date: Date) => setStartDate(date)}
+                           setEndDate={(date: Date) => setEndDate(date)} dates={dates}/>
+                <FeedbackDetails dates={dates} feedbacks={feedbacks}/>
+            </ScrollView>
         </View>
     )
 }
